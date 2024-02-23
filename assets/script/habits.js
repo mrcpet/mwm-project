e875cadc0654d459e9795cf0acaed0ba8b01273d
wlhlmkhl <143789953+wlhlmkhl@users.noreply.github.com>
2024-02-23 09:20:17 +0100

    backup push

--- assets/script/habits.js
+++ assets/script/habits.js
@@ -1,11 +1,15 @@
+// Globals
 const addHabitsBtn = document.querySelector("#add-button");
 const inputField = document.querySelector("#input-field");
 const prioDropDown = document.querySelector("#habit-prio");
 const listEl = document.querySelector("#habit-list");
+const sortBtn = document.querySelector("#sort-btn");
+const checkboxes = document.querySelectorAll("[name = 'filter']");
 let myList = [];
+let trueOrFalse = false;
 
+// Kod-block 1
 const listFromLocalStorage = JSON.parse(localStorage.getItem("habits"));
-let user = "kevin";
 
 if (listFromLocalStorage) {
   myList = listFromLocalStorage;
@@ -17,12 +21,33 @@ addHabitsBtn.addEventListener("click", () => {
   renderData(myList);
 });
 
+sortBtn.addEventListener("click", () => {
+  trueOrFalse = !trueOrFalse;
+  sortByPrio(myList, trueOrFalse);
+});
+addCheckboxEventListeners(checkboxes);
+
+// Funktioner
+function sortByPrio(array, boolean) {
+  let newList;
+  if (boolean) {
+    newList = array.sort((a, b) => {
+      return b.prio - a.prio;
+    });
+  } else {
+    newList = array.sort((a, b) => {
+      return a.prio - b.prio;
+    });
+  }
+  renderData(newList);
+}
+
 function addAndStoreHabit() {
-  habit = {};
+  let habit = {};
   habit.title = inputField.value;
   habit.prio = prioDropDown.value;
   myList.push(habit);
-  localStorage.setItem("habits", JSON.stringify(myList));
+  updateLocalStorage(myList);
   clearInput(inputField, prioDropDown);
 }
 
@@ -32,21 +57,55 @@ function renderData(array) {
     let newLi = document.createElement("li");
     newLi.textContent = habit.title;
     newLi.dataset.title = habit.title;
-    newLi.addEventListener("click", () => {
-      const foundItem = myList.find((item) => {
-        return item.title === habit.title;
-      });
-
-      let index = myList.indexOf(foundItem);
-
-      console.log(index);
-      //   newLi.remove();
+    newLi.dataset.prio = habit.prio;
+    newLi.addEventListener("click", (event) => {
+      deleteHabit(event.target.textContent);
+      cl(event.target);
     });
     listEl.append(newLi);
   });
 }
 
+function cl(a) {
+  console.log(a);
+}
+
+function deleteHabit(habit) {
+  const foundItem = myList.find((item) => item.title === habit);
+  let index = myList.indexOf(foundItem);
+  myList.splice(index, 1);
+  updateLocalStorage(myList);
+  renderData(myList);
+}
+
 function clearInput(input, dropdown) {
   input.value = "";
   dropdown.selectedIndex = 0;
+}
+function updateLocalStorage(array) {
+  localStorage.setItem("habits", JSON.stringify(array));
+}
+function addCheckboxEventListeners(checkBoxes) {
+  checkBoxes.forEach((checkBox) => {
+    checkBox.addEventListener("change", () => {
+      const selectedFilters = document.querySelectorAll(
+        "[name='filter']:checked"
+      );
+      let activeFilters = [];
+
+      selectedFilters.forEach((filter) => {
+        activeFilters.push(filter.value);
+      });
+
+      // let filteredPokemons = PokedexData.filter((pokemon) => {
+      //   return pickedTypes.includes(pokemon.type);
+      // });
+
+      // let filteredHabits = myList.filter((habit)=>{
+      //   return activeFilters.includes()
+      // })
+
+      cl(activeFilters);
+    });
+  });
 }
