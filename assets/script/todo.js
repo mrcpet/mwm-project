31500e34d9e793df5dc745958649448e952b4747
Ejenjard <madeleine.ejenfjard@gmail.com>
2024-02-26 15:20:54 +0100

    Added logic for creating a todo and rendering todos in a separate div.

--- assets/script/todo.js
+++ assets/script/todo.js
@@ -1,20 +1,25 @@
 let createTodo = () => {
     let createTodoBtn = document.getElementById("icon_addtodo");
+    let todoListContainer = document.getElementById("container-todos");
     let todoInput = document.getElementById("todoInput");
 
     createTodoBtn.addEventListener('click', () => {
         // Visar diven där en ny todo skapas
         todoInput.style.display = 'block';
+        console.log("Du klickade på att skapa en todo!")
+
+
+        // Dölj todoListContainer när användaren väljer att skapa en ny todo
+        todoListContainer.style.display = 'none';
+
+        // Rensa inputfälten och select-fältet
+        document.getElementById("todoName").value = "";
+        document.getElementById("todoCategory").selectedIndex = 0;
+        document.getElementById("todoDeadline").value = "";
+        document.getElementById("todoDuration").value = "";
     });
 
-    let addTodo = () => {
-        let addTodoButton = document.getElementById("addTodoButton"); 
 
-        addTodoButton.addEventListener('click', () => {
-            //logik för att skicka Todon till sidan där alla todos hamnar
-        })
-
-    }
 };
 
 // Anropa createTodo-funktionen när sidan laddas
@@ -22,14 +27,48 @@ createTodo();
 
 
 
+let saveAndRenderTodo = () => {
+    let addTodoBtn = document.getElementById("addTodoButton");
+
+    addTodoBtn.addEventListener('click', () => {
+        let todoNameValue = document.getElementById("todoName").value;
+        let todoCategoryValue = document.getElementById("todoCategory").value;
+        let todoDeadlineValue = document.getElementById("todoDeadline").value;
+        let todoDurationValue = document.getElementById("todoDuration").value;
+
+        let todo = {
+            name: todoNameValue,
+            category: todoCategoryValue,
+            deadline: todoDeadlineValue,
+            duration: todoDurationValue
+        };
+
+        renderTodo(todo); // Anropa renderTodo med den nya todo för att visa den i listan
+    });
+};
+
+let renderTodo = (todo) => {
+    let todoListContainer = document.getElementById("container-todos");
+
+    todoListContainer.style.display = 'block';
+
+    let todoInput = document.getElementById("todoInput");
+    todoInput.style.display = 'none';
+
+    let todoList = document.getElementById("todoList");
+
+    let todoItem = document.createElement("li");
+
+    todoItem.textContent = todo.name; // Använd textContent för att sätta innehållet till att endast visa namnet på todo
+
+    todoList.appendChild(todoItem);
+};
 
 
 
 
 
-
-
-
+saveAndRenderTodo();
 
 
 
