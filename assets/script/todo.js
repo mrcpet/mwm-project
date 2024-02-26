b8e6e153ce1e3abc3a5a2d38e858b316e5a1614e
Ejenjard <madeleine.ejenfjard@gmail.com>
2024-02-26 12:46:31 +0100

    Added a function to create a todo

--- assets/script/todo.js
+++ assets/script/todo.js
@@ -1,3 +1,24 @@
+let createTodo = () => {
+    let createTodoBtn = document.getElementById("icon_addtodo");
+    let todoInput = document.getElementById("todoInput");
+
+    createTodoBtn.addEventListener('click', () => {
+        // Visar diven där en ny todo skapas
+        todoInput.style.display = 'block';
+    });
+
+    let addTodo = () => {
+        let addTodoButton = document.getElementById("addTodoButton"); 
+
+        addTodoButton.addEventListener('click', () => {
+            //logik för att skicka Todon till sidan där alla todos hamnar
+        })
+
+    }
+};
+
+// Anropa createTodo-funktionen när sidan laddas
+createTodo();
 
 
 
@@ -5,10 +26,16 @@
 
 
 
-let addTodoBtn = document.getElementById("icon_addtodo");
 
-function addTodo() {
-    console.log('Knappen klickades!');
-}
 
-addTodoBtn.addEventListener('click', addTodo);
+
+
+
+
+
+
+
+
+
+
+
