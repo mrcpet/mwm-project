8203fcbcf4702a2f5f538161c508e9cf065b4ad0
wlhlmkhl <143789953+wlhlmkhl@users.noreply.github.com>
2024-03-06 10:17:41 +0100

    jobba i fil branch en hel dag :S

--- assets/script/pomodoro.js
+++ assets/script/pomodoro.js
@@ -1,22 +1,79 @@
-// Set the end date for the timer. ...
-// Make the timer function update every second. ...
-// Calculate the remaining time in days, hours, minutes, and seconds. ...
-// ​Display the output to users. ...
-// Display a message when the timer is over. ...
-// Implementation.
+const timerDisplayEl = document.querySelector("#timer-display");
+const startPauseBtn = document.querySelector("#start-pause-button");
+const restartBtn = document.querySelector("#restart-button");
+const pomodoroBtn = document.querySelector("#pomodoro");
+const shortBreakBtn = document.querySelector("#short-break");
+const longBreakBtn = document.querySelector("#long-break");
+const customTimeBtn = document.querySelector("#custom-time");
+let selectedtime = 0; // ska alltid vara i sekunder
+let countDown;
+let isTimerRunning = false;
 
-const pomodoro = 25 * 60;
-const shortBreak = 5 * 60;
-const longBreak = 15 * 60;
+pomodoroBtn.addEventListener("click", () => {
+  handleButtonClick(pomodoroBtn.value);
+});
+shortBreakBtn.addEventListener("click", () => {
+  handleButtonClick(shortBreakBtn.value);
+});
+longBreakBtn.addEventListener("click", () => {
+  handleButtonClick(longBreakBtn.value);
+});
+startPauseBtn.addEventListener("click", () => {
+  isTimerRunning ? stopTimer(countDown) : startTimer(selectedtime);
+  isTimerRunning = !isTimerRunning;
+  changeIconSet(isTimerRunning);
+});
+restartBtn.addEventListener("click", () => {});
 
-function timer(seconds) {
-  let time = seconds;
-  const countDown = setInterval(() => {
-    console.log(time);
-    time--;
-    if (time < 0) {
+//--------------Funktioner--------------
+
+// funktion omvandlar sekunder till rätt format och uppdaterar DOMen.
+function updateDisplay(time) {
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  timerDisplayEl.innerHTML = `${minutes}:${seconds}`;
+}
+// Hämtar värdet från kanppen och deklarerar global variabel.
+function handleButtonClick(value) {
+  selectedtime = transformToSeconds(value);
+  updateDisplay(selectedtime);
+}
+
+// Omvandlar minute till sekunder.
+function transformToSeconds(minutes) {
+  return minutes * 60;
+}
+// Startar timer med värde och avslutar vid 0.
+function startTimer(time) {
+  let seconds = time;
+  countDown = setInterval(() => {
+    updateDisplay(seconds);
+    seconds--;
+    selectedtime = seconds;
+
+    if (seconds < 0) {
       clearInterval(countDown);
     }
-  }, 1000);
+  }, 100);
 }
-// timer(shortBreak);
+// Stoppar intervallen och
+function stopTimer(interval) {
+  clearInterval(interval);
+}
+function removeHidden(elem) {
+  elem.classList.remove("hidden");
+}
+function addHidden(elem) {
+  elem.classList.add("hidden");
+}
+
+function changeIconSet(boolean) {
+  boolean
+    ? ((startPauseBtn.innerHTML =
+        '<img src="/assets/icons/Icon/pomodoro/Icon/Outline/pause-circle.svg" />'),
+      removeHidden(restartBtn))
+    : ((startPauseBtn.innerHTML =
+        '<img src="/assets/icons/Icon/pomodoro/Icon/Outline/play-circle.svg" />'),
+      addHidden(restartBtn));
+}
